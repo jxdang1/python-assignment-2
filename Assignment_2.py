80928ba52b5460f81bcb0320ff22aecccf82372d
Jennifer Dang <144502402+jxdang1@users.noreply.github.com>
2025-08-26 16:16:16 -0500

    Add files via upload
    create dynamic variables that stores the current grade, create function that calculates the average, create input for validating positive integer is being entered

--- Assignment_2.py
+++ Assignment_2.py
@@ -10,40 +10,48 @@ print("*************************************\n")
 print("""Purpose: This program calculates the average grade from
 the user's entered grades and determines a letter grade to it.\n""")
 
-# This stores the grades into a dictionary without making it a list or array
-
-# This stores the grades into a dictionary without making it a list or array
 
 def calculate_gradebook():
-    student_grades = {}
+    grade_sum = 0
+    grade_count = 0
 
-    total_grade_sum = 0
+# Prompt user for how many numbers of grades that will be inputed
 
-    entered_grade_num = 0
+num_grades = input("Enter the number of grades: ")
+
+# Validate input to ensure it's a positive integer
+try:
+    num_grades = int(num_grades_input)
+    if num_grade <= 0:
+        print("Please enter a positive integer.")
+        return
+except ValueError:
+    print("Invalid input. Please enter a positive integer.")
+    return
+
+amount_grades = int(num_grades)
 
 
-# Loop that will ask User to enter a valid input 
+for i in range(1, num_grades + 1):
+    while True:
+        try:
+            #Use a dynamic variable to store the current grade
+            start_grade = int(input(f"Enter grade #{i}: "))
+            grade_sum += start_grade
+            grade_count += 1
+            break # Exit the inner loop on valid input
+        except ValueError:
+                print("Invalid input. Please enter an integer.")
+                    
 
-while True:
-    try:
-        grade_input() = input("Enter the number of grades: ")
 
-        if grade_input  == "":
-            print("No input found.")
-            return # Will exit function if no enter input was found
-        
-        entered_grade_num = int(grade_input)
-        
-        if entered_grade_num < 0:
-            print("Number cannot be negative. Try again.")
-        else:
-            break
-    except ValueError:
-        print("Invald input. Enter a whole number for number of grades.")
+# Calculates the average
 
-        if entered_grade_num == 0:
-            print("Zero grades were entered.")
-            return
+if grade_count > 0:
+    average = grade_sum / grade_count
+else:
+    average = 0.0
+
 
 # Condtional statement for final letter grade based on outcome of grade numbers
 
@@ -62,4 +70,4 @@ else:
 
 print("The final letter grade is:", letter_grade)
 
-calculate_gradebook():
+calculate_gradebook()
